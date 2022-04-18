a6ff5ba83709d801a817bdde2ada7b08cb9d881e
Elena Gamboni <95409842+Elena-6363@users.noreply.github.com>
2022-04-18 22:57:46 +0200

    Update Presentation.sol

--- Basket and ERC20/Presentation.sol
+++ Basket and ERC20/Presentation.sol
@@ -3,7 +3,7 @@
 pragma solidity 0.8.9;
 
 
-import './ERC721.sol';
+//import './ERC20.sol';
 import './AXNFT.sol';
 
 contract NFTStaking is  AXNFT {  
@@ -17,38 +17,33 @@ contract NFTStaking is  AXNFT {
   vaultInfo[] public VaultInfo;
    
   // struct to store a stake's token, owner, and earning values
-  struct Basket {
+  struct Basket_1 {
     uint24 tokenId;
    
   }
 
-  AXNFT nft; 
-   
+  AXNFT nft;
+  //ERC20 token; 
+
+  uint256  weightbar = 10000;
   uint256 public totalStaked;
-  mapping(uint256 => Basket) public vault; 
+  mapping(uint256 => Basket_1) public vault_1; 
   event NFTpacked(address owner, uint256 tokenId);
   
 
-  function packing_Basket_1(uint256 _pid, uint256 /*calldata*/ tokenId) public {
-      //unterschied zwischen external und public view 
-    vaultInfo storage vaultid = VaultInfo[_pid]; //ist das wirklich nötig?
-  
-      // kann man funktion package 1 , package 2 nennen sodass Fehlermeldung sinn macht?
+  function packing_Basket_1(uint256 _pid, uint256 tokenId) public {
+    
     require(vaultid.nft.ownerOf(tokenId) == msg.sender, "not your token");
-    require(vault[tokenId].tokenId == 0, 'already staked');
-    //var (string memory URL, string memory aXedrasId, string memory finessbar, uint256 weightbar, string memory provenancebar, string memory materialbar, string memory certificationbar) = vaultid.nft.tokendata(tokenId);
+    require(vault_1[tokenId].tokenId == 0, "already staked");
     //var (URL, aXedrasId, finessbar, weightbar, provenancebar, materialbar, certificationbar) = vaultid.nft.tokendata(tokenId);
-
     //require(certificationbar == "" && provenancebar == "" && materialbar = "", "Not the right basket.);
-      // mit require muss jeder Smart Contract schon alle möglichen Basket beachtet haben.
     
     vaultid.nft.transferFrom(msg.sender, address(this), tokenId);
     emit NFTpacked(msg.sender, tokenId);
-    //NToken = weightbar;
-      //uint j = 0; j < NToken; j++) {
-      //vaultid.token.mint();
-    //}
-    vault[tokenId] = Basket({
+    uint256 NToken = weightbar;
+    //vaultid.token.mint(address(this), NToken);
+    
+    vault_1[tokenId] = Basket_1({
       tokenId: uint24(tokenId)
     });
     
