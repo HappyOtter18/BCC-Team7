cae8eef6f997496b7f8c283c13635fb519092cf2
Elena Gamboni <95409842+Elena-6363@users.noreply.github.com>
2022-04-18 22:25:28 +0200

    Update Presentation.sol

--- Basket and ERC20/Presentation.sol
+++ Basket and ERC20/Presentation.sol
@@ -1,15 +1,45 @@
-function packing_Basket_1(uint256 _pid, uint256 /*calldata*/ tokenId) public {
+// SPDX-License-Identifier: MIT LICENSE
+
+pragma solidity 0.8.9;
+
+
+import './ERC721.sol';
+import './AXNFT.sol';
+
+contract NFTStaking is  AXNFT {  
+
+//braucht es wirklich beide structs?
+  struct vaultInfo {
+        AXNFT nft;
+        string name;  //will ich eigentlich nicht ausklammern da eher wichtig, aber ansonsten gibt es eine Warnung
+  }
+
+  vaultInfo[] public VaultInfo;
+   
+  // struct to store a stake's token, owner, and earning values
+  struct Basket {
+    uint24 tokenId;
+   
+  }
+
+  AXNFT nft; 
+   
+  uint256 public totalStaked;
+  mapping(uint256 => Basket) public vault; 
+  event NFTpacked(address owner, uint256 tokenId);
+  
+
+  function packing_Basket_1(uint256 _pid, uint256 /*calldata*/ tokenId) public {
       //unterschied zwischen external und public view 
     vaultInfo storage vaultid = VaultInfo[_pid]; //ist das wirklich nötig?
-    
-    //require(vaultInfo = /*bestimmte vaultID*/, "not correct vault") //keine gute Idee da man ja nicht sagen kann welche Function verwendet wird ->Lisa
+  
       // kann man funktion package 1 , package 2 nennen sodass Fehlermeldung sinn macht?
     require(vaultid.nft.ownerOf(tokenId) == msg.sender, "not your token");
     require(vault[tokenId].tokenId == 0, 'already staked');
     //var (string memory URL, string memory aXedrasId, string memory finessbar, uint256 weightbar, string memory provenancebar, string memory materialbar, string memory certificationbar) = vaultid.nft.tokendata(tokenId);
     //var (URL, aXedrasId, finessbar, weightbar, provenancebar, materialbar, certificationbar) = vaultid.nft.tokendata(tokenId);
 
-    //require(certificationbar == "" && provenancebar == "" && materialbar = "", "not  );
+    //require(certificationbar == "" && provenancebar == "" && materialbar = "", "Not the right basket.);
       // mit require muss jeder Smart Contract schon alle möglichen Basket beachtet haben.
     
     vaultid.nft.transferFrom(msg.sender, address(this), tokenId);
@@ -23,3 +53,4 @@ function packing_Basket_1(uint256 _pid, uint256 /*calldata*/ tokenId) public {
     });
     
   }
+}
