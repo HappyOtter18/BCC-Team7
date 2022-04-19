9bb1eb9bacff511a5fc20536e4401f4e6f98ffff
HappyOtter18 <95071880+HappyOtter18@users.noreply.github.com>
2022-04-19 09:55:18 +0200

    Update constructor and mint function
    Only aX can now mint the token (assumed they are the deployer of the contract)
    The constructor had to be adapted

--- ERC 721 aXedras/AXNFT.sol
+++ ERC 721 aXedras/AXNFT.sol
@@ -19,11 +19,21 @@ contract AXNFT is connector {
     mapping(string => bool) _AXNFTzExists;
 
 
+    address aX;
+
+//initialize this contract to inherit name and symbol form erc721metadata so that the name is goldnft and the symbol is AXNFT
+    constructor () connector("GoldNFT", "AXNFT") {
+        aX = msg.sender;
+    }
+
+
+
 // This function will make an array of the NFT already minted
 // Mint NFT to adresses and keep track of the adresses who mint the NFTs. 
 //The other mint funtion, we don't have access directly, this is internal
     function mint(string memory _url, string memory _aXuniqueId, string memory _finess, uint256 _weightGramms, string memory _provenance, string memory _material, string memory _certification) public {
         require(!_AXNFTzExists[_url], "error, this NFT already exists"); //We can't mint two same NFTs
+        require (tx.origin == aX, "you can't mint NFTs on this contract"); //Only the company, deployer of the contract can mint the token
         AXNFTz.push(_url);
         aXUniqueId.push(_aXuniqueId);
         finess.push(_finess);
@@ -32,9 +42,13 @@ contract AXNFT is connector {
         material.push(_material);
         certification.push(_certification);
         uint _id = AXNFTz.length -1;
+        _origin[_id] = _provenance;
+        _mat[_id] = _material;
+        _certif[_id]= _certification;
         _mint(msg.sender, _id); //we get an id for each token. The msg.sender is our adress (the one that interact). 
         _AXNFTzExists[_url] =true;
     }
+
 
     function tokendata(uint256 _tokenId) public view returns (string memory, string memory, string memory, uint256, string memory, string memory, string memory){
         string memory URL = AXNFTz[_tokenId];
