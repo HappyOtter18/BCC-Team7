60ccf3f9dab9c37727a506c36107bb6b7d51751f
Elena Gamboni <95409842+Elena-6363@users.noreply.github.com>
2022-05-06 19:09:44 +0200

    Update SMART CONTRACT NFTBASKET_1.sol

--- Basket and ERC20/SMART CONTRACT NFTBASKET_1.sol
+++ Basket and ERC20/SMART CONTRACT NFTBASKET_1.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 import './minting_1.sol';
 
+//interface to get the functions of the AXNFT Smart Contract
 interface DataInterface {
   function tokendata(uint256 _tokenId) external view returns (
     string memory, 
@@ -28,31 +29,57 @@ interface DataInterface {
   function approve(address to, uint256 tokenId) external;
 }
 
-
+//NFT Basket Smart Contract inherit from the Smart Contract minting_1
 contract NFTBasket is minting_1 {  
   
   DataInterface NFTContract;
   
 
-  constructor (address AXNFT) { 
+  constructor (address AXNFT) {   //so that we can use always the current address of the AXNFT Smart Contract
     NFTContract = DataInterface(AXNFT);
     
   }
 
+  //struct so that we can see what the data of the NFTs is. 
+  //I used only this data because the other data is already a criterium for the basket
+  // and with all the data it would be to large.
   struct Basket_1 {
     uint256 tokenId;
     uint256 totalinBasket;
     uint256 totalmintToken;
+    uint256 weightbar;
+    string aXedrasId;
+    string finessbar;
+    string URL;
   }
 
+  uint[] public basket_1_array;  //makes Array, so we can easily iterate,
+  //and see how many nfts are in the basket and what NFTs are in the Baskets
+
+  function addNFTtoArray(uint256 tokenId) private {  //funciton to extend array
+    basket_1_array.push(tokenId);
+  }
+
+  function lengthofArray()public view returns (uint){  //funciton to get the length of array
+    return basket_1_array.length;
+  }
+
+  //initializes the variables
   uint256 public totalmintToken = 0;
   uint256 public totalinBasket = 0;
-  mapping(uint256 => Basket_1) public basket_1; 
+  mapping(uint256 => Basket_1) public basket_1;  //maps the struct
   event NFTpacked(address owner, uint256 tokenId);
   
+
+  //the main function of the smart contract 
+  //it takes a NFT, controlls if it is a right one and pack it into the Basket 1
+  //Then it sends per gramm of weight a token to the owner of the NFT
+  //before that you must give a approve to the Smart Contract and to the executor of the function because it controls about that 
+  //and the smart contract becomes the owner inside of the transferFrom function
   function packing_Basket_1(uint256 tokenId, address _from) public {
     require(NFTContract.ownerOf(tokenId) == _from, "not your token"); 
     require(basket_1[tokenId].tokenId == 0, 'already staked');
+    //initialize the variables
     string memory URL;
     string memory aXedrasId;
     string memory finessbar;
@@ -63,26 +90,39 @@ contract NFTBasket is minting_1 {
     (URL, aXedrasId, finessbar, weightbar, provenancebar, materialbar, certificationbar) = NFTContract.tokendata(tokenId);
     string memory a = "Available";
     string memory r = "Recycled";
+    //controlls if it is the right basket
     require(keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((certificationbar))), "Not the right basket.");
     require(keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((provenancebar))), "Not the right basket.");
     require(keccak256(abi.encodePacked((r))) == keccak256(abi.encodePacked((materialbar))) , "Not the right basket."); 
     
-    NFTContract.transferFrom(_from, address(this), tokenId);
-    totalinBasket = totalinBasket + 1;
+    NFTContract.transferFrom(_from, address(this), tokenId); //sending the NFTs
+    totalinBasket = totalinBasket + 1; //to know how many NFTs are in the Basket
     emit NFTpacked(_from, tokenId);
     uint256 NToken = weightbar;
     mint(_from, NToken);
-    totalmintToken = totalmintToken + NToken;
+    totalmintToken = totalmintToken + NToken; //to know how many Token are circulating
+    addNFTtoArray(tokenId);  //add to the array
 
     basket_1[tokenId] = Basket_1({
       tokenId: uint24(tokenId),
       totalinBasket: uint256(totalinBasket),
-      totalmintToken: uint256(totalmintToken)
+      totalmintToken: uint256(totalmintToken),
+      weightbar: uint256(weightbar),
+      finessbar: string(finessbar),
+      URL: string(URL),
+      aXedrasId: string(aXedrasId)
     });
   }
 
+  //funcion to remove the now empty index of the array
+  function remove(uint tokenId) private{
+    basket_1_array[tokenId] = basket_1_array[basket_1_array.length - 1];
+    basket_1_array.pop();
+  }
   
-  function unpack_NFT(address from, uint256 amount) public {
+  // funciton to get a NFT or various NFTs from the basket for a specific amount of Token
+  function unpack_NFT_1(address from, uint256 amount) public {
+    //initialize the variables
     string memory aXedrasId;
     string memory finessbar;
     uint256 weightbar;
@@ -90,41 +130,25 @@ contract NFTBasket is minting_1 {
     string memory materialbar;
     string memory certificationbar;
     string memory URL;
-    while(0 < amount){
-      for (uint i = 0; i < totalinBasket; i++) {
-        uint24 tokenId = uint24(basket_1[i].tokenId);
-        (URL, aXedrasId, finessbar, weightbar, provenancebar, materialbar, certificationbar) = NFTContract.tokendata(tokenId);
-        if (weightbar <= amount) {
-          transferto(from, address(this), weightbar);
-          burnToken(address(this), weightbar);
-          amount = amount - weightbar;
-          NFTContract.transferFrom(address(this),  from, tokenId);
-          delete basket_1[tokenId];
-        }
+    require(amount >= 0, "not enough token");
+    for (uint i = 0; i < basket_1_array.length; i++) {
+      uint24 tokenId = uint24(basket_1_array[i]);
+      (URL, aXedrasId, finessbar, weightbar, provenancebar, materialbar, certificationbar) = NFTContract.tokendata(tokenId);
+      if (weightbar <= amount) {
+        burnfrom(from, weightbar);
+        amount = amount - weightbar;
+        NFTContract.transferFrom(address(this),  from, tokenId); 
+        delete basket_1[tokenId];
+        remove(tokenId);
+        totalinBasket= totalinBasket-1;
+        totalmintToken = totalmintToken-weightbar;
       }
     }
   }
 
-
-  //https://ethereum.stackexchange.com/questions/7317/how-can-i-return-struct-when-function-is-called
-  function showNFT() public view returns(string memory, string memory, string memory, uint256, string memory, string memory, string memory) {
-    string memory output="";
-    string memory aXedrasId;
-    string memory finessbar;
-    uint256 weightbar;
-    string memory provenancebar;
-    string memory materialbar;
-    string memory certificationbar;
-    string memory URL;
-    for (uint i = 0; i < totalinBasket; i++) {
-        output = string(abi.encodePacked(output,"[", basket_1[i].tokenId, ",", basket_1[i].totalinBasket, ",", basket_1[i].totalmintToken, ",""]"));
-        uint tokenId = basket_1[i].tokenId;
-        (URL, aXedrasId, finessbar, weightbar, provenancebar, materialbar, certificationbar) = NFTContract.tokendata(tokenId);
-    }
-  return (output, URL, finessbar, weightbar, provenancebar, materialbar, certificationbar);
-  }
-
-  function get_specificNFT(uint tokenId, uint amount, address from) public {
+  //function to get the NFT from the basket with a specific Token Id
+  function get_specificNFT_1(uint tokenId, uint amount, address from) public {
+    //initialize the variables
     string memory aXedrasId;
     string memory finessbar;
     uint256 weightbar;
@@ -135,11 +159,14 @@ contract NFTBasket is minting_1 {
     (URL, aXedrasId, finessbar, weightbar, provenancebar, materialbar, certificationbar) = NFTContract.tokendata(tokenId);
     uint weight = weightbar;
     require(weight<=amount, "not enough token");
-    transferto(from, address(this), weightbar);
-    burnToken(address(this), amount);
+    burnfrom(from, weight);
     NFTContract.transferFrom(address(this),  from, tokenId);
     delete basket_1[tokenId];
+    remove(tokenId);
+    totalinBasket= totalinBasket-1;
+    totalmintToken = totalmintToken-weightbar;
   }
 
 }
+
 
